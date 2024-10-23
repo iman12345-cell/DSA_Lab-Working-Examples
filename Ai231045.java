7b99e072a02617ac70af8111a06be5b3b75bb167
iman12345-cell <imanmirza300@gmail.com>
2024-10-22 21:40:21 -0700

    Add files via upload

---
+++ Ai231045.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ai231045;
+
+/**
+ *
+ * @author 123
+ */
+public class Ai231045 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1 ; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of first 10 Natural Numbers = " + sum);
+    }
+}
