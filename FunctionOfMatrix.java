a06821bb691b83fbc4f3306572df357566770a27
iman12345-cell <imanmirza300@gmail.com>
2024-11-14 07:25:07 -0800

    Add files via upload

---
+++ FunctionOfMatrix.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.insertvars;
+
+/**
+ *
+ * @author Tesla Laptops
+ */
+import java.util.Arrays;
+
+public class FunctionOfMatrix {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int leftDiagonalSum = calculateLeftDiagonalSum(matrix);
+        int rightDiagonalSum = calculateRightDiagonalSum(matrix);
+
+        System.out.println("Left Diagonal Sum: " + leftDiagonalSum);
+        System.out.println("Right Diagonal Sum: " + rightDiagonalSum);
+    }
+
+    public static int calculateLeftDiagonalSum(int[][] matrix) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            sum += matrix[i][i];
+        }
+        return sum;
+    }
+
+
+    public static int calculateRightDiagonalSum(int[][] matrix) {
+        int sum = 0;
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            sum += matrix[i][n - 1 - i]; 
+        }
+        return sum;
+    }
+}
